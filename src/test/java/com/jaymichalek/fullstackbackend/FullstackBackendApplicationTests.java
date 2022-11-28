88a8e97473d7bb33cb78f2920934c98b0331f817
jmichalek <jaycoinminer@gmail.com>
2022-11-27 17:19:32 -0700

    Initial commit

---
+++ src/test/java/com/jaymichalek/fullstackbackend/FullstackBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jaymichalek.fullstackbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullstackBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
